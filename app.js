09ac444ff89d1d6b31c78ca0dddfd075d6800e81
Natasha Wilson <natasha.wilson@triptease.com>
2023-03-01 16:40:36 +0000

    add env file

---
+++ app.js
@@ -0,0 +1,6 @@
+const hello = 'hello'
+
+
+
+//posts a message in a channel
+//curl -X POST -F channel="developing-slack-apps" -F text="Testing testing acrobuster" https://slack.com/api/chat.postMessage -H "Authorization: Bearer xoxb"
