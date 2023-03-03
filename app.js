f8f80272249d57c2853b64f1a5dbda9c57af4ab9
Natasha Wilson <natasha.wilson@triptease.com>
2023-03-03 15:04:39 +0000

    Make app respond to listener

--- app.js
+++ app.js
@@ -1,6 +1,31 @@
-const hello = 'hello'
+require('dotenv').config()
+
+const { App } = require('@slack/bolt');
+
+// Initializes your app with your bot token and signing secret
+const app = new App({
+    token: process.env.SLACK_BOT_TOKEN,
+    signingSecret: process.env.SLACK_SIGNING_SECRET,
+    socketMode: true,
+    appToken: process.env.SLACK_APP_TOKEN
+});
+
+
+app.message('hello', async ({ message, say}) => {
+    await say(`Hey there <@${message.user}>!`);
+});
+
+app.message
 
 
 
-//posts a message in a channel
-//curl -X POST -F channel="developing-slack-apps" -F text="Testing testing acrobuster" https://slack.com/api/chat.postMessage -H "Authorization: Bearer xoxb"
+
+
+
+
+(async () => {
+    // Start your app
+    await app.start(process.env.PORT || 3000);
+
+    console.log('⚡️ Bolt app is running!');
+})();
